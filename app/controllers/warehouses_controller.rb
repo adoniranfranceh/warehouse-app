9f8d2a8ccb61b30e8f41cecb8ca1868bc88f922f
Adoniran França <adoniranfranca@gmail.com>
2023-10-20 12:27:21 -0300

    removendo repetições

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,48 +1,46 @@
 class WarehousesController < ApplicationController
-
-  def show
-    id = params[:id]
-    @warehouse = Warehouse.find(id)
-  end
+  before_action :set_warehouse, only: [:show, :edit, :update]
+  def show; end
 
   def new
     @warehouse = Warehouse.new
   end
 
   def create
-    warehouse_params = params.require(:warehouse).permit(:name, :description, :code, :address,
-                                                         :city, :cep, :area)
-
     @warehouse = Warehouse.new(warehouse_params)
 
     if @warehouse.save
       redirect_to root_path, notice: 'Galpão cadastrado com sucesso.'
     else
       flash.now[:notice] = 'Galpão não cadastrado.'
-      render :new
+      render 'new'
     end
   end
 
-  def edit
-    id = params[:id]
-    @warehouse = Warehouse.find(id)
-  end
+  def edit; end
 
   def update
-    id = params[:id]
-    @warehouse = Warehouse.find(id)
-
-    warehouse_params = params.require(:warehouse).permit(:name, :description, :code, :address,
-                                                         :city, :cep, :area)
     if @warehouse.update(warehouse_params)
       redirect_to warehouse_path(@warehouse.id), notice: 'Galpão atualizado com sucesso.'
     else
-      flash.now[:notice] = 'Não foi possivel atualizar o galpão. lalal'
+      flash.now[:notice] = 'Não foi possivel atualizar o galpão.'
       render 'edit'
     end
   end
 
+  private
+
   def set_warehouse
     @warehouse = Warehouse.find(params[:id])
+  end
+
+  def warehouse_params
+    params.require(:warehouse).permit(:name,
+                                      :description,
+                                      :code,
+                                      :address,
+                                      :city,
+                                      :cep,
+                                      :area)
   end
 end
