c2bdb1aa1511dbdaa7344fbb66d7d2901b9ea48f
Adoniran França <adoniranfranca@gmail.com>
2023-10-22 18:19:15 -0300

    Garantido validações para modelos de produto

--- app/controllers/product_models_controller.rb
+++ app/controllers/product_models_controller.rb
@@ -13,6 +13,10 @@ class ProductModelsController < ApplicationController
 
     if @product_model.save
       redirect_to @product_model, notice: 'Modelo de produto cadastrado com sucesso.'
+    else
+      @suppliers = Supplier.all
+      flash.now[:notice] = 'Não foi possível cadastrar o modelo de produto.'
+      render 'new'
     end
   end
 
--- spec/system/user_register_product_model_spec.rb
+++ spec/system/user_register_product_model_spec.rb
@@ -6,6 +6,9 @@ describe 'Usuário cadastra um modelo de produto' do
   	supplier = Supplier.create!(brand_name: 'Samsung', corporate_name: 'Samsung Eletronicos LTDA',
   														registration_number: '56144734000107',full_address: 'Av Nacoes Unida, 1000', 
   														city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
+    other_supplier = Supplier.create!(brand_name: 'LG', corporate_name: 'LG do Brasil LTDA',
+                              registration_number: '24785893000196',full_address: 'Av Ibirapuera, 300', 
+                              city: 'São Paulo', state: 'SP', email: 'contato@lg.com.br')
   	# Act
   	visit root_path
   	click_on 'Modelos de Produto'
@@ -27,5 +30,23 @@ describe 'Usuário cadastra um modelo de produto' do
   	expect(page).to have_content 'SKU: TV40-SAMS-XPTO'
   	expect(page).to have_content 'Dimensão: 60cm x 90cm x 10cm'
   	expect(page).to have_content 'Peso: 10000g'
+  end
+
+  it 'e deve preencher todos os campos' do
+    # Arrange
+    supplier = Supplier.create!(brand_name: 'Samsung', corporate_name: 'Samsung Eletronicos LTDA',
+                              registration_number: '56144734000107',full_address: 'Av Nacoes Unida, 1000', 
+                              city: 'São Paulo', state: 'SP', email: 'sac@samsung.com.br')
+    # Act
+    visit root_path
+    click_on 'Modelos de Produto'
+    click_on 'Cadastrar Novo'
+
+    fill_in 'Nome', with: ''
+    fill_in 'SKU', with: ''
+    click_on 'Enviar'
+
+    # Assert
+    expect(page).to have_content 'Não foi possível cadastrar o modelo de produto.'
   end
 end
