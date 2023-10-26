7ffe96fbce3e0641bd3c400f9b9f669f950fb69d
Adoniran França <adoniranfranca@gmail.com>
2023-10-26 18:38:14 -0300

    Usuário pode buscar pedidos

--- spec/system/orders/user_search_order_spec.rb
+++ spec/system/orders/user_search_order_spec.rb
@@ -47,4 +47,36 @@ describe 'Usuário buscar por pedido' do
   	expect(page).to have_content('Galpão Destino: SDU - Rio')
   	expect(page).to have_content('Fornecedor: Spark Industries Brasil LTDA - 24785893000196')
   end
+
+  it 'e encontra múltiplos pedidos' do
+    # Arrange
+    user = User.create!(name: 'Sergio', email: 'sergio@email.com', password: 'password')
+    first_warehouse = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000,
+                                        address: 'Avenida do Aeroporto, 1000', cep: '15000-00',
+                    description: 'Galpão destinado para cargas internacionais')
+    second_warehouse = Warehouse.create!(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000,
+                                        address: 'Av do  Porto, 1000', cep: '20000-000', description: 'Galpão do Rio')
+    supplier = Supplier.create!(corporate_name: 'Spark Industries Brasil LTDA', brand_name: 'Spark', registration_number: '24785893000196',
+                                full_address: 'Torre da Indústria, 1', city: 'Teresina', state: 'PI', email: 'vendedor@gmail.com')
+    allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('GRU12345')
+    first_order = Order.create(user: user, warehouse: first_warehouse, supplier: supplier, estimated_delivery_date: 1.day.from_now)
+    allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('GRU98765')
+    first_order = Order.create(user: user, warehouse: first_warehouse, supplier: supplier, estimated_delivery_date: 1.day.from_now)
+    allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('SDU00000')
+    first_order = Order.create(user: user, warehouse: second_warehouse, supplier: supplier, estimated_delivery_date: 1.day.from_now)
+
+    # Act
+    login_as(user)
+    visit root_path
+    fill_in 'Buscar Pedidos', with: 'GRU'
+    click_on 'Buscar'
+
+    # Assert
+    expect(page).to have_content('2 pedidos encontrados')
+    expect(page).to have_content('Resultado da busca por: GRU')
+    expect(page).to have_content('Código: GRU12345')
+    expect(page).to have_content('Código: GRU98765')
+
+    # Assert
+  end
 end
