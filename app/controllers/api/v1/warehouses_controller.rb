48ec24e4f8de650e2170895c97a7042a417c7ec7
Adoniran França <adoniranfranca@gmail.com>
2023-11-10 22:22:32 -0300

    APIs: Criando galpões e tratando erros

--- app/controllers/api/v1/warehouses_controller.rb
+++ app/controllers/api/v1/warehouses_controller.rb
@@ -1,15 +1,27 @@
-class Api::V1::WarehousesController < ActionController::API
+class Api::V1::WarehousesController < Api::V1::ApiController
   def show
-    begin
-      warehouse = Warehouse.find(params[:id])
-      render status: 200, json: warehouse.as_json(except: [:created_at, :updated_at])
-    rescue
-      render status: 404
-    end
+    warehouse = Warehouse.find(params[:id])
+    render status: 200, json: warehouse.as_json(except: [:created_at, :updated_at])
   end
 
   def index
     warehouses = Warehouse.all.order(:name)
     render status: 200, json: warehouses
+  end
+
+  def create
+    warehouse_params = params.require(:warehouse).permit(:name,
+                                                         :description,
+                                                         :code,
+                                                         :address,
+                                                         :city,
+                                                         :cep,
+                                                         :area)
+    warehouse = Warehouse.new(warehouse_params)
+    if warehouse.save
+      render status: 201, json: warehouse
+    else
+      render status: 512, json: { errors: warehouse.errors.full_messages }
+    end
   end
 end
